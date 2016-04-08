9252657ed639e93a4bfb8cfb2be420604b72290a
Chris Hurst <chris@iamchrishurst.com>
2016-04-08 16:10:29 -0400

    Updated functions file

--- functions.php
+++ functions.php
@@ -119,9 +119,6 @@ function mdlpress_scripts() {
 
 	wp_enqueue_style( 'material-icons', '//fonts.googleapis.com/icon?family=Material+Icons' );
 
-
-	wp_enqueue_script( 'mdlpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'mdlpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'mdl-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array(), '1.0.0', true );
